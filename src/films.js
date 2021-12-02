58a19556b6b43dfa3f1018197af2ddfa51299c2c
Kike Cabrera <enriquecabrera92@gmail.com>
2021-12-02 01:00:21 +0100

    Exercici 1

--- src/films.js
+++ src/films.js
@@ -1,7 +1,9 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result =  array.map(function(movie){
+    return movie.director;
+  });
+  
   return result;
 }
 
